f763181950a28346ad108a93116d9500569e99c8
lab56 <lab56@fatec.com>
2024-11-18 10:32:35 -0300

    mensagem

---
+++ src/test/java/com/bd/sitebd/SitebdApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bd.sitebd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitebdApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+		}
+
+}
